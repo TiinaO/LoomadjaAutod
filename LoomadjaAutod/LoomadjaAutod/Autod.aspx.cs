c60414752c284c502ff811014ce664e630acede5
Tiina Onemar <tiina.onemar@gmail.com>
2016-06-16 13:38:05 +0300

    Esimene ülesanne

---
+++ LoomadjaAutod/LoomadjaAutod/Autod.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoomadjaAutod
+{
+    public partial class Autod : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DetailsView1_PageIndexChanging(object sender, DetailsViewPageEventArgs e)
+        {
+
+        }
+    }
+}
